4ac27fb209a636ca486ba48b02f4ac0c9ba37c2f
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2018-02-01 13:17:32 +1100

    alchol sensor code for MQ-3 module

--- breathalyser/breathalyser.ino
+++ breathalyser/breathalyser.ino
@@ -1,7 +1,16 @@
 #include <ESP8266WiFi.h>
 
-char wifi_ssid[] = "<SSID>";
-char wifi_password[] = "<PASSWORD>";
+#include <ESP8266WiFi.h>
+
+char wifi_ssid[] = "NETGEAR63";
+char wifi_password[] = "chummyroad556";
+
+
+const int AOUTpin=0;//the AOUT pin of the alcohol sensor goes into analog pin A0 of the arduino
+const int DOUTpin=8;//the DOUT pin of the alcohol sensor goes into digital pin D8 of the arduino
+const int ledPin=13;//the anode of the LED connects to digital pin D13 of the arduino
+
+int barValue;
 
 void setup() {
   Serial.begin(115200);
@@ -20,9 +29,25 @@ void setup() {
 
   Serial.println();
   Serial.println("Setup complete");
+
+  pinMode(DOUTpin, INPUT);//sets the pin as an input to the arduino
+  pinMode(ledPin, OUTPUT);//sets the pin as an output of the arduino
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  barValue = analogRead(AOUTpin);//reads the analaog value from the alcohol sensor's AOUT pin
+  Serial.println(barValue);
+ 
+  if (barValue < 200) {
+      Serial.println("You are sober.");
+  } else if (barValue >= 200 && barValue < 280) {
+      Serial.println("Alcohol detected");
+  } else if (barValue >= 280 && barValue < 350) {
+      Serial.println("More than one drink going on here....");
+  } else if (barValue >= 350 && barValue < 450) {
+      Serial.println("Serious Booze here! ");
+  } else if(barValue > 450) {
+     Serial.println("You are drunk as fart!");
+  }
 
 }
